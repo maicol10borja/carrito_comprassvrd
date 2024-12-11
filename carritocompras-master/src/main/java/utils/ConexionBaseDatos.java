14e3b1654cbd9c2251c77013152688d98c57c458
maicol10borja <166356848+maicol10borja@users.noreply.github.com>
2024-12-10 12:21:09 -1200

    Update ConexionBaseDatos.java

--- carritocompras-master/src/main/java/utils/ConexionBaseDatos.java
+++ carritocompras-master/src/main/java/utils/ConexionBaseDatos.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBaseDatos {
-    private static String url="jdbc:mysql://localhost:3306/carro?serverTimezone=UTC";
+    private static String url="jdbc:mysql://localhost:3306/ejemplocarro?serverTimezone=UTC";
     private static String username="root";
     private static String password="";
 
